f08caeda3bb0f334a78ee286c8e8de47a6e527de
Shruti3004 <mail2shruti.ag@gmail.com>
2023-04-19 08:40:13 +0530

    feat: added tailwind support
    Signed-off-by: Shruti3004 <mail2shruti.ag@gmail.com>

---
+++ apps/web/pages/_app.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Web from ".";
+import "./styles/tailwind.css";
+import "./styles/global.css";
+
+const Index = () => {
+  return <Web />;
+};
+
+export default Index;
---
+++ packages/ui/components/Button.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export const Button = () => {
+  return (
+    <>
+      <button className="rounded bg-[#DB6027] py-2 px-10 font-bold text-white hover:bg-[#c0440b]">
+        Login
+      </button>
+    </>
+  );
+};
