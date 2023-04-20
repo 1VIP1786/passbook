facf6f364581350dec82b076a49633e97f5afdf0
Shruti3004 <mail2shruti.ag@gmail.com>
2023-04-20 09:28:24 +0530

    fix: folder structure
    Signed-off-by: Shruti3004 <mail2shruti.ag@gmail.com>

--- apps/passbook/src/pages/_app.tsx
+++ apps/passbook/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Web from ".";
-import "./styles/tailwind.css";
-import "./styles/global.css";
+import "../styles/tailwind.css";
+import "../styles/global.css";
 
 const Index = () => {
   return <Web />;
