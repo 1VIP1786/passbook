5fa5b642a04ac037ed733e6568f66ab7166bb162
Shruti3004 <mail2shruti.ag@gmail.com>
2023-04-24 16:10:47 +0530

    feat: home screen
    Signed-off-by: Shruti3004 <mail2shruti.ag@gmail.com>

--- apps/passbook/src/pages/home/index.tsx
+++ apps/passbook/src/pages/home/index.tsx
@@ -4,10 +4,35 @@ import Navbar from "../../components/navbar";
 
 const Home = () => {
   return (
-    <div className="min-h-screen flex justify-center items-center">
+    <div className="min-h-screen">
       <Navbar />
       <Header />
-      <div className="font-medium text-[20px]">Home</div>
+      <div className="bg-primary py-4 px-3 mt-44 mx-5 rounded-lg text-white">
+        <h1 className="font-demi text-[20px]">XXXXXXXXX8734</h1>
+        <div className="font-regular text-[11px]">FAMILY ID</div>
+
+        <h1 className="font-demi text-[20px] mt-2">NARAJIYA</h1>
+        <div className="font-regular text-[11px]">Bilariaganj, Azamgarh</div>
+        <div className="flex justify-between mt-3">
+          <div className="font-medium text-[11px]">#7 members</div>
+          <div className="font-regular text-[11px]">FY 2022-23</div>
+        </div>
+      </div>
+      <div className="text-primary font-demi mt-4 text-center underline text-[18px]">
+        Availed
+      </div>
+      <div className="bg-linear-gradient py-4 px-3 mt-4 mx-5 rounded-lg text-white text-center">
+        <h1 className="font-bold text-[24px]">Rs 96,937</h1>
+        <div className="font-regular text-[11px]">
+          Benefits availed from the government schemes{" "}
+        </div>
+      </div>
+      <div className="py-2 px-3 mt-4 mx-5 rounded-lg text-center bg-schemes-availed font-regular uppercase">
+        <span className="text-secondary font-bold text-[24px]">
+          23
+        </span>{" "}
+        <span className="align-text-bottom">Schemes Availed</span>
+      </div>
       <Bottombar />
     </div>
   );
