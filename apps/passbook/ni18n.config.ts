1df630868bc3c41c1ea1aa163256517e58762085
Shruti3004 <mail2shruti.ag@gmail.com>
2023-06-02 23:09:57 +0530

    fix: localization
    Signed-off-by: Shruti3004 <mail2shruti.ag@gmail.com>

--- apps/passbook/ni18n.config.ts
+++ apps/passbook/ni18n.config.ts
@@ -1,5 +1,15 @@
 const supportedLngs = ["en", "hi"];
 export const ni18nConfig = {
   supportedLngs,
-  ns: ["common"],
+  ns: [
+    "common",
+    "benefits",
+    "bottomBar",
+    "comingSoon",
+    "family",
+    "familyDetails",
+    "home",
+    "otp",
+    "sidebar",
+  ],
 };
